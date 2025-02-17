c820ef7d09e14b74d17826b66b7d1960741e3f9f
Wondong Shin <wodshin@gmail.com>
2025-02-17 15:00:51 +0900

    fix: Change binding address to 0.0.0.0 for Docker support

--- src/main.rs
+++ src/main.rs
@@ -116,7 +116,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .route("/convert", web::get().to(convert_and_resize_image))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
